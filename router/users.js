92967f48aa231536d03fc32622c884da6fa4c5ac
Your Name <you@example.com>
2022-02-06 16:03:04 +0330

    Ref : Error handlig chash to tow request

--- router/users.js
+++ router/users.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../models/users')
 const bcrypt = require('bcrypt')
+const Post = require('../models/posts')
 //Update
 router.put('/:id',async(req,res)=>{
     if(req.body.userId === req.params.id){
@@ -20,4 +21,28 @@ res.status(200).json(updateUser)
         res.status(401).json("you can update only accunt")
     }
 })
+
+
+//Delete
+
+router.delete('/:id',async(req,res)=>{
+    if(req.body.userId === req.params.id){
+        try{
+            const user = await User.findById(req.params.id)
+            try{
+await Post.deleteMany({username:user.username})
+await User.findByIdAndDelete(req.params.id)
+res.status(200).json('user has been deleted...')
+            }catch(err){
+                res.status(500).json(err)
+            }
+        }catch(err){
+res.status(404).json('not found user')
+        }
+    }
+})
+
+
+
+
 module.exports = router
